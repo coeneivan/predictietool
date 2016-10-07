7788bf9f7d9637af58469d77d16e809ce0ef77d3
coeneivan <ivan.coene@student.vives.be>
2016-10-07 12:16:03 +0200

    Test

--- ForecastVB/MainScreen.Designer.vb
+++ ForecastVB/MainScreen.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class MainScreen
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,81 +20,23 @@ Partial Class MainScreen
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
-        Me.txtJaar = New System.Windows.Forms.TextBox()
         Me.cboSubAfd = New System.Windows.Forms.ComboBox()
         Me.cboLesdag = New System.Windows.Forms.ComboBox()
         Me.dtpStartcursus = New System.Windows.Forms.DateTimePicker()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.txtResultMerk = New System.Windows.Forms.TextBox()
-        Me.txtRestultJaar = New System.Windows.Forms.TextBox()
         Me.txtResultSubAfd = New System.Windows.Forms.TextBox()
         Me.txtRestultLesDag = New System.Windows.Forms.TextBox()
         Me.txtResultDatum = New System.Windows.Forms.TextBox()
+        Me.cboMerk = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtRestultJaar = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(199, 157)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(66, 25)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Merk:"
-        '
-        'RadioButton1
-        '
-        Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(316, 118)
-        Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(149, 29)
-        Me.RadioButton1.TabIndex = 1
-        Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Tag = "merk"
-        Me.RadioButton1.Text = "Syntrawest"
-        Me.RadioButton1.UseVisualStyleBackColor = True
-        '
-        'RadioButton2
-        '
-        Me.RadioButton2.AutoSize = True
-        Me.RadioButton2.Location = New System.Drawing.Point(316, 153)
-        Me.RadioButton2.Name = "RadioButton2"
-        Me.RadioButton2.Size = New System.Drawing.Size(89, 29)
-        Me.RadioButton2.TabIndex = 1
-        Me.RadioButton2.TabStop = True
-        Me.RadioButton2.Tag = "merk"
-        Me.RadioButton2.Text = "SBM"
-        Me.RadioButton2.UseVisualStyleBackColor = True
-        '
-        'RadioButton3
-        '
-        Me.RadioButton3.AutoSize = True
-        Me.RadioButton3.Location = New System.Drawing.Point(316, 188)
-        Me.RadioButton3.Name = "RadioButton3"
-        Me.RadioButton3.Size = New System.Drawing.Size(108, 29)
-        Me.RadioButton3.TabIndex = 1
-        Me.RadioButton3.TabStop = True
-        Me.RadioButton3.Tag = "merk"
-        Me.RadioButton3.Text = "Escala"
-        Me.RadioButton3.UseVisualStyleBackColor = True
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(205, 269)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(60, 25)
-        Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Jaar:"
         '
         'Label3
         '
@@ -123,20 +65,13 @@ Partial Class MainScreen
         Me.Label5.TabIndex = 5
         Me.Label5.Text = "Start van de cursus:"
         '
-        'txtJaar
-        '
-        Me.txtJaar.Location = New System.Drawing.Point(316, 266)
-        Me.txtJaar.Name = "txtJaar"
-        Me.txtJaar.Size = New System.Drawing.Size(311, 31)
-        Me.txtJaar.TabIndex = 6
-        '
         'cboSubAfd
         '
         Me.cboSubAfd.FormattingEnabled = True
         Me.cboSubAfd.Location = New System.Drawing.Point(316, 378)
         Me.cboSubAfd.Name = "cboSubAfd"
         Me.cboSubAfd.Size = New System.Drawing.Size(311, 33)
-        Me.cboSubAfd.TabIndex = 7
+        Me.cboSubAfd.TabIndex = 1
         '
         'cboLesdag
         '
@@ -144,14 +79,14 @@ Partial Class MainScreen
         Me.cboLesdag.Location = New System.Drawing.Point(316, 490)
         Me.cboLesdag.Name = "cboLesdag"
         Me.cboLesdag.Size = New System.Drawing.Size(311, 33)
-        Me.cboLesdag.TabIndex = 8
+        Me.cboLesdag.TabIndex = 2
         '
         'dtpStartcursus
         '
         Me.dtpStartcursus.Location = New System.Drawing.Point(316, 605)
         Me.dtpStartcursus.Name = "dtpStartcursus"
         Me.dtpStartcursus.Size = New System.Drawing.Size(311, 31)
-        Me.dtpStartcursus.TabIndex = 9
+        Me.dtpStartcursus.TabIndex = 3
         '
         'Label6
         '
@@ -165,44 +100,72 @@ Partial Class MainScreen
         '
         'txtResultMerk
         '
-        Me.txtResultMerk.Location = New System.Drawing.Point(66, 755)
+        Me.txtResultMerk.Location = New System.Drawing.Point(66, 784)
         Me.txtResultMerk.Name = "txtResultMerk"
+        Me.txtResultMerk.ReadOnly = True
         Me.txtResultMerk.Size = New System.Drawing.Size(561, 31)
         Me.txtResultMerk.TabIndex = 11
-        '
-        'txtRestultJaar
-        '
-        Me.txtRestultJaar.Location = New System.Drawing.Point(66, 792)
-        Me.txtRestultJaar.Name = "txtRestultJaar"
-        Me.txtRestultJaar.Size = New System.Drawing.Size(561, 31)
-        Me.txtRestultJaar.TabIndex = 11
+        Me.txtResultMerk.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'txtResultSubAfd
         '
-        Me.txtResultSubAfd.Location = New System.Drawing.Point(66, 829)
+        Me.txtResultSubAfd.Location = New System.Drawing.Point(66, 821)
         Me.txtResultSubAfd.Name = "txtResultSubAfd"
+        Me.txtResultSubAfd.ReadOnly = True
         Me.txtResultSubAfd.Size = New System.Drawing.Size(561, 31)
         Me.txtResultSubAfd.TabIndex = 11
+        Me.txtResultSubAfd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'txtRestultLesDag
         '
-        Me.txtRestultLesDag.Location = New System.Drawing.Point(66, 866)
+        Me.txtRestultLesDag.Location = New System.Drawing.Point(66, 858)
         Me.txtRestultLesDag.Name = "txtRestultLesDag"
+        Me.txtRestultLesDag.ReadOnly = True
         Me.txtRestultLesDag.Size = New System.Drawing.Size(561, 31)
         Me.txtRestultLesDag.TabIndex = 11
+        Me.txtRestultLesDag.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'txtResultDatum
         '
-        Me.txtResultDatum.Location = New System.Drawing.Point(66, 903)
+        Me.txtResultDatum.Location = New System.Drawing.Point(66, 895)
         Me.txtResultDatum.Name = "txtResultDatum"
+        Me.txtResultDatum.ReadOnly = True
         Me.txtResultDatum.Size = New System.Drawing.Size(561, 31)
         Me.txtResultDatum.TabIndex = 11
+        Me.txtResultDatum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'cboMerk
+        '
+        Me.cboMerk.FormattingEnabled = True
+        Me.cboMerk.Location = New System.Drawing.Point(316, 266)
+        Me.cboMerk.Name = "cboMerk"
+        Me.cboMerk.Size = New System.Drawing.Size(311, 33)
+        Me.cboMerk.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(205, 269)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 25)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Merk:"
+        '
+        'txtRestultJaar
+        '
+        Me.txtRestultJaar.Location = New System.Drawing.Point(66, 747)
+        Me.txtRestultJaar.Name = "txtRestultJaar"
+        Me.txtRestultJaar.ReadOnly = True
+        Me.txtRestultJaar.Size = New System.Drawing.Size(561, 31)
+        Me.txtRestultJaar.TabIndex = 11
+        Me.txtRestultJaar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'MainScreen
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(712, 991)
+        Me.Controls.Add(Me.cboMerk)
         Me.Controls.Add(Me.txtResultDatum)
         Me.Controls.Add(Me.txtRestultLesDag)
         Me.Controls.Add(Me.txtResultSubAfd)
@@ -212,38 +175,28 @@ Partial Class MainScreen
         Me.Controls.Add(Me.dtpStartcursus)
         Me.Controls.Add(Me.cboLesdag)
         Me.Controls.Add(Me.cboSubAfd)
-        Me.Controls.Add(Me.txtJaar)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.RadioButton3)
-        Me.Controls.Add(Me.RadioButton2)
-        Me.Controls.Add(Me.RadioButton1)
-        Me.Controls.Add(Me.Label1)
         Me.Name = "MainScreen"
         Me.Text = "Cursus realiseerbaarheid prospectie tool"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents Label1 As Label
-    Friend WithEvents RadioButton1 As RadioButton
-    Friend WithEvents RadioButton2 As RadioButton
-    Friend WithEvents RadioButton3 As RadioButton
-    Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
-    Friend WithEvents txtJaar As TextBox
     Friend WithEvents cboSubAfd As ComboBox
     Friend WithEvents cboLesdag As ComboBox
     Friend WithEvents dtpStartcursus As DateTimePicker
     Friend WithEvents Label6 As Label
     Friend WithEvents txtResultMerk As TextBox
-    Friend WithEvents txtRestultJaar As TextBox
     Friend WithEvents txtResultSubAfd As TextBox
     Friend WithEvents txtRestultLesDag As TextBox
     Friend WithEvents txtResultDatum As TextBox
+    Friend WithEvents cboMerk As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtRestultJaar As TextBox
 End Class
--- ForecastVB/MainScreen.vb
+++ ForecastVB/MainScreen.vb
@@ -1,3 +1,59 @@
 ﻿Public Class MainScreen
+    Private Sub MainScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim s As New SQLUtil
+        'Load all cursussen
+        Dim subAfds = s.Execute("SELECT distinct CodeSubafdeling FROM Cursussen")
+        For Each row As Object In subAfds
+            cboSubAfd.Items.Add(row.ToString)
+        Next
 
+        'Load dagen
+        Dim dagen = {"Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag"}
+        For Each row As Object In dagen
+            cboLesdag.Items.Add(row.ToString)
+        Next
+
+        'Load merken
+        Dim merken = s.Execute("SELECT distinct merk FROM Cursussen")
+        For Each row As Object In merken
+            cboMerk.Items.Add(row.ToString)
+        Next
+
+        'Calculate main percentage
+        Dim knownX, knownY As New ArrayList
+        knownY.Add(79.55)
+        knownY.Add(75.93)
+        knownY.Add(75.75)
+        knownY.Add(77.53)
+        knownY.Add(75.5)
+        knownY.Add(75.91)
+        knownY.Add(76.3)
+        knownY.Add(74.55)
+        knownY.Add(71.06)
+        knownX.Add(2006)
+        knownX.Add(2007)
+        knownX.Add(2008)
+        knownX.Add(2009)
+        knownX.Add(2010)
+        knownX.Add(2011)
+        knownX.Add(2012)
+        knownX.Add(2013)
+        knownX.Add(2014)
+        Dim p As New Prospect
+        Dim range = p.certainty(knownY, p.prospect(knownY, knownX, CDbl(2015)))
+        txtRestultJaar.Text = "[" + Math.Round(range(0), 2).ToString + " - " + Math.Round(range(1), 2).ToString + "]"
+    End Sub
+
+    Private Sub cboSubAfd_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboSubAfd.SelectedIndexChanged
+        Dim s As New SQLUtil
+        Dim subAfdsAll = s.Execute("SELECT Opleidingsnr FROM Cursussen WHERE CodeSubafdeling = '" + cboSubAfd.SelectedItem + "'")
+        Dim subAfdsGeschrapt = s.Execute("SELECT Opleidingsnr FROM Cursussen WHERE CodeSubafdeling = '" + cboSubAfd.SelectedItem + "'and codeIngetrokken ='ja'")
+        Dim knownX = s.Execute("SELECT Distinct YEAR(startdatum) FROM Cursussen WHERE CodeSubafdeling = '" + cboSubAfd.SelectedItem + "'and year(StartDatum)<2015 ")
+        Dim knownY = s.Execute("")
+
+        Dim p As New Prospect
+        Dim range = p.certainty(knownY, p.prospect(knownY, knownX, CDbl(2015)))
+        txtResultSubAfd.Text = "[" + Math.Round(range(0), 2).ToString + " - " + Math.Round(range(1), 2).ToString + "]"
+
+    End Sub
 End Class
