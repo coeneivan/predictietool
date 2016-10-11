1a9f34f5bfc5ae3bb9140a0c66ae38092d0980ff
blackterror25 <benborra@hotmail.com>
2016-10-11 17:20:12 +0200

    Enkele graphics uitwerken in R

--- RProject/script.R
+++ RProject/script.R
@@ -1,6 +1,34 @@
-
 library(RODBC)
-dbhandle <- odbcDriverConnect('driver=SQL Server;server=laptop-BEN_ASUS;database=SyntraTest')
-year <- sqlQuery(dbhandle, 'SELECT DISTINCT year(startdatum), count(codeingetrokken FROM [SyntraTest].[dbo].[Cursussen] where codeingetrokken = \'Nee\'')
+library(ggvis)
 
-year
+source("dbconnection.R")
+
+inget <- sqlQuery(dbhandle, "SELECTFROM [SyntraTest].[dbo].[Cursussen] where codeingetrokken = \'Nee\' GROUP BY year(startdatum)")
+
+
+
+DrawNietGeschraptPerJaar <- function() {
+    year <- sqlQuery(dbhandle, "SELECT DISTINCT year(startdatum) as jaar, count(codeingetrokken) as NietGeschrapteCursussen FROM [SyntraTest].[dbo].[Cursussen] where codeingetrokken = \'Nee\' GROUP BY year(startdatum)")
+    plot(year$jaar, year$NietGeschrapteCursussen, "l")
+}
+
+# Filter op subafdeling, wanneer subafdeling == null, wordt alles terug gegeven.
+DrawVerhoudingGeschraptPerDagPerSubafdeling <- function(Subafdeling) {
+    q1 = 
+    query = sprintf("SELECT DISTINCT dag, count(*) as totaal, COUNT(CASE WHEN CodeIngetrokken = 'Nee' THEN 1 ELSE NULL END) AS Doorgegaan, COUNT(CASE WHEN CodeIngetrokken = 'Ja' THEN 1 ELSE NULL END) AS Ingetrokken from Cursussen where CodeSubafdeling = \'%s\' Group by dag;", Subafdeling)
+    # Run Query
+    week <- sqlQuery(dbhandle, query)
+    week2 <- data.frame(week)
+    barplot(as.matrix(week2))   
+}
+
+# Filter op subafdeling, wanneer subafdeling == null, wordt alles terug gegeven.
+DrawVerhoudingGeschraptPerDag <- function() {
+    quer = "SELECT DISTINCT dag, count(*) as totaal, COUNT(CASE WHEN CodeIngetrokken = 'Nee' THEN 1 ELSE NULL END) AS Doorgegaan, COUNT(CASE WHEN CodeIngetrokken = 'Ja' THEN 1 ELSE NULL END) AS Ingetrokken from Cursussen Group by dag;"
+    
+    # Run Query
+    week <- sqlQuery(dbhandle, quer)
+    barplot(week)
+}
+
+DrawVerhoudingGeschraptPerDagPerSubafdeling("WEB")
