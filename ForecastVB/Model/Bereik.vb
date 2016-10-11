af423e94d7fce5f58ee5c384f9f7a6a6b55d9df3
coeneivan <ivan.coene@student.vives.be>
2016-10-11 14:37:46 +0200

    Docummentatie aangevuld, houden zo ;)

--- ForecastVB/Model/Bereik.vb
+++ ForecastVB/Model/Bereik.vb
@@ -1,10 +1,23 @@
-﻿Public Class Bereik
+﻿''' <summary>
+''' Simpele klasse om bereik van verwachtingen weer te geven
+''' </summary>
+Public Class Bereik
     Private ondergrens, average, bovengrens As Double
+    ''' <summary>
+    ''' Initialiseert de klasse
+    ''' </summary>
+    ''' <param name="pOndergrens">Double met de waarde van de ondergrens</param>
+    ''' <param name="pAverage">Double met de waarde van het gemiddelde</param>
+    ''' <param name="pBovengrens">Double met de waarde van de bovengrens</param>
     Public Sub New(pOndergrens As Double, pAverage As Double, pBovengrens As Double)
         ondergrens = pOndergrens
         bovengrens = pBovengrens
         average = pAverage
     End Sub
+    ''' <summary>
+    ''' Een stringweergave van het bereik
+    ''' </summary>
+    ''' <returns>Een string om aan de eindgebruike te tonen</returns>
     Public Overrides Function ToString() As String
         Return "[" + Math.Round(ondergrens, 2).ToString + " - " + Math.Round(average, 2).ToString + " - " + Math.Round(bovengrens, 2).ToString + "]"
     End Function
