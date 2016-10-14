412278c1d948b2400c81e3826cb5144dd3272b91
blackterror25 <benborra@hotmail.com>
2016-10-14 13:29:15 +0200

    Fixed?³

--- ForecastVB/MainScreen.vb
+++ ForecastVB/MainScreen.vb
@@ -66,4 +66,9 @@
     Public Function getFilters() As ArrayList
         Return filterlist
     End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim t As New Test
+        t.Show()
+    End Sub
 End Class
