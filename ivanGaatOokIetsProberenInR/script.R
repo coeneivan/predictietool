6708935ce5d8694117457d05f17d17593e5c5dc1
blackterror25 <benborra@hotmail.com>
2016-10-15 11:20:26 +0200

    Tis kappot

--- ivanGaatOokIetsProberenInR/script.R
+++ ivanGaatOokIetsProberenInR/script.R
@@ -7,10 +7,10 @@ library(pROC)
 library(RODBC)
 library(randomForest)
 
-dbhandle <- odbcDriverConnect('driver=SQL Server;server=USER-PC\\SQLExpress;database=SyntraTest')
+dbhandle <- odbcDriverConnect('driver=SQL Server;server=laptop-BEN_ASUS;database=SyntraTest')
 
-inget <- sqlQuery(dbhandle, "SELECT CodeIngetrokken, month(StartDatum) as StartDatum, dag, Merk, UitvCentrum, CodeSoortCursus, CodeAnalytischPlanSubafdeling, CodeSubafdeling, OpInternet FROM [SyntraTest].[dbo].[Cursussen]")
-colnames(inget) <- c("CodeIngetrokken", "StartDatum", "dag", "Merk", "UitvCentrum") #"CodeSoortCursus", "CodeAnalytischPlanSubafdeling", "CodeSubafdeling", "OpInternet"
+inget <- sqlQuery(dbhandle, "SELECT TOP 10 CodeIngetrokken, month(StartDatum) as StartDatum, dag, Merk, UitvCentrum, CodeSoortCursus, CodeAnalytischPlanSubafdeling, CodeSubafdeling, OpInternet FROM [SyntraTest].[dbo].[Cursussen] Where Aard NOT IN (15,29,14,58,12,13) and OmschrijvingComm not like '%SELOR%' and OmschrijvingComm not like '%Bekwaamheidsattest%' and OmschrijvingComm not like '%stage%' and OmschrijvingComm not like '%proef%' and OmschrijvingComm not like '%attest%' and OmschrijvingComm not like '%aanvullende praktijk%' and OmschrijvingComm not like '%eindwerk%' and OmschrijvingComm not like '%AP%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'POC%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'C.I.%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'P.S.%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'LeReN%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'Lerend%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'Iedereen leert%' and OmschrijvingComm not like 'Bedrijvig Brugge%' and OmschrijvingComm not like '%testavond%' and OmschrijvingComm not like 'Talent%' and OmschrijvingComm not like '%Spoor 1%' and OmschrijvingComm not like '%Spoor 2%' and OmschrijvingComm not like '%Spoor 3%' and OmschrijvingComm not like '%Spoor 4%' and OmschrijvingComm not like '%Spoor 5%'")
+colnames(inget) <- c("CodeIngetrokken", "StartDatum", "dag", "Merk", "UitvCentrum","CodeSoortCursus", "CodeAnalytischPlanSubafdeling", "CodeSubafdeling", "OpInternet")
 #summary(inget)
 
 ############VOOR OVER DE MIDDAG
@@ -18,7 +18,7 @@ colnames(inget) <- c("CodeIngetrokken", "StartDatum", "dag", "Merk", "UitvCentru
 # prepare training scheme
 control <- trainControl(method = "repeatedcv", number = 10, repeats = 3)
 # train the model
-model <- train(CodeIngetrokken ~ StartDatum + dag + Merk + UitvCentrum, data = inget, method = "lvq", preProcess = "scale", trControl = control)
+model <- train(CodeIngetrokken ~ ., data = inget, method = "lvq", preProcess = "scale", trControl = control)
 # estimate variable importance
 importance <- varImp(model, scale = FALSE)
 
