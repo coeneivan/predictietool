7ed251f1fb13a216e30dc429c096af992ba9371a
blackterror25 <benborra@hotmail.com>
2016-10-17 15:51:41 +0200

    R update

--- ivanGaatOokIetsProberenInR/script.R
+++ ivanGaatOokIetsProberenInR/script.R
@@ -7,10 +7,12 @@ library(pROC)
 library(RODBC)
 library(randomForest)
 
+start = Sys.time()
 dbhandle <- odbcDriverConnect('driver=SQL Server;server=laptop-BEN_ASUS;database=SyntraTest')
 
-inget <- sqlQuery(dbhandle, "SELECT CodeIngetrokken, month(StartDatum) as StartDatum, dag, Merk, UitvCentrum, CodeSoortCursus, CodeAnalytischPlanSubafdeling, CodeSubafdeling, OpInternet FROM [SyntraTest].[dbo].[Cursussen] Where Aard NOT IN (15,29,14,58,12,13) and OmschrijvingComm not like '%SELOR%' and OmschrijvingComm not like '%Bekwaamheidsattest%' and OmschrijvingComm not like '%stage%' and OmschrijvingComm not like '%proef%' and OmschrijvingComm not like '%attest%' and OmschrijvingComm not like '%aanvullende praktijk%' and OmschrijvingComm not like '%eindwerk%' and OmschrijvingComm not like '%AP%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'POC%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'C.I.%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'P.S.%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'LeReN%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'Lerend%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'Iedereen leert%' and OmschrijvingComm not like 'Bedrijvig Brugge%' and OmschrijvingComm not like '%testavond%' and OmschrijvingComm not like 'Talent%' and OmschrijvingComm not like '%Spoor 1%' and OmschrijvingComm not like '%Spoor 2%' and OmschrijvingComm not like '%Spoor 3%' and OmschrijvingComm not like '%Spoor 4%' and OmschrijvingComm not like '%Spoor 5%'")
-colnames(inget) <- c("CodeIngetrokken", "StartDatum", "dag", "Merk", "UitvCentrum","CodeSoortCursus", "CodeAnalytischPlanSubafdeling", "CodeSubafdeling", "OpInternet")
+
+inget <- sqlQuery(dbhandle, "SELECT top 100 CodeIngetrokken, month(StartDatum) as StartDatum, dag, Merk, UitvCentrum, CodeSoortCursus, CodeSubafdeling, OpInternet FROM [SyntraTest].[dbo].[Cursussen] Where Aard NOT IN (15,29,14,58,12,13) and OmschrijvingComm not like '%SELOR%' and OmschrijvingComm not like '%Bekwaamheidsattest%' and OmschrijvingComm not like '%stage%' and OmschrijvingComm not like '%proef%' and OmschrijvingComm not like '%attest%' and OmschrijvingComm not like '%aanvullende praktijk%' and OmschrijvingComm not like '%eindwerk%' and OmschrijvingComm not like '%AP%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'POC%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'C.I.%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'P.S.%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'LeReN%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'Lerend%' Collate SQL_Latin1_General_CP1_CS_AS and OmschrijvingComm not like 'Iedereen leert%' and OmschrijvingComm not like 'Bedrijvig Brugge%' and OmschrijvingComm not like '%testavond%' and OmschrijvingComm not like 'Talent%' and OmschrijvingComm not like '%Spoor 1%' and OmschrijvingComm not like '%Spoor 2%' and OmschrijvingComm not like '%Spoor 3%' and OmschrijvingComm not like '%Spoor 4%' and OmschrijvingComm not like '%Spoor 5%'")
+colnames(inget) <- c("CodeIngetrokken", "StartDatum", "dag", "Merk", "UitvCentrum", "CodeSoortCursus", "CodeSubafdeling", "OpInternet")
 #summary(inget)
 
 ############VOOR OVER DE MIDDAG
@@ -27,6 +29,8 @@ print(importance)
 # plot importance
 plot(importance)
 
+print("TOTAL: ")
+print(difftime(Sys.time(), start))
 # define the control using a random forest selection function
 #control <- rfeControl(functions = rfFuncs, method = "repeatedcv", repeats = 5,verbose = FALSE)
 # run the RFE algorithm
