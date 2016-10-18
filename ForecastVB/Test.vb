adb19a91f8888744741a294e0ba3e369e508acdb
coeneivan <ivan.coene@student.vives.be>
2016-10-18 15:26:30 +0200

    3

--- ForecastVB/Test.vb
+++ ForecastVB/Test.vb
@@ -23,18 +23,58 @@
     End Sub
 
     Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
-        Try
-            startUp()
-            Dim sql As New SQLUtil
-            Dim subAfds As New ArrayList
-            Dim subBll As New subAfdBll
-            Dim ja = 0
-            Dim boven = 0
-            Dim nee = 0
-            Dim filters = root.getFilters
+        startUp()
+        Dim sql As New SQLUtil
+        Dim subAfds As New ArrayList
+        Dim subBll As New subAfdBll
+        Dim ja = 0
+        Dim nee = 0
+        Dim filters = root.getFilters
+        If filters Is Nothing Then
+            subAfds.AddRange(sql.getArrayList("select distinct CodeSubafdeling from Cursussen where Merk = '" + cboMerk.SelectedItem.ToString + "' and dag='" + cboDag.SelectedItem.ToString + "' and year(StartDatum) = 2015 group by codesubafdeling having count(*) > 5").ToArray())
+            'TODO: catch empty arraylist
+        Else
+            Dim fil As String
+            fil = ""
+
+            For Each filIt As FilterItem In filters
+                fil += " AND " + filIt.kolom + " " + filIt.factor + " " + filIt.filter
+            Next
+            subAfds.AddRange(sql.getArrayList("select distinct CodeSubafdeling from Cursussen where Merk = '" + cboMerk.SelectedItem.ToString + "' and dag='" + cboDag.SelectedItem.ToString + "' and year(StartDatum) = 2015 " + fil + "group by codesubafdeling having count(*) > 5").ToArray())
+
+        End If
+        pgb.Minimum = 0
+        If subAfds.Count = 0 Then
+            pgb.Maximum = 1
+        Else
+            pgb.Maximum = subAfds.Count - 1
+        End If
+
+        For i As Integer = 0 To subAfds.Count - 1
+            Dim merk As New MerkBLL
+            Dim dag As New DagBll
+            Dim lvi As New ListViewItem(subAfds(i).ToString)
+            Dim subA As Bereik = subBll.berekenVerwachtingsBereikVoorSubAfd(2015, subAfds(i), Nothing)
+            Dim dagA As Bereik = dag.berekenVerwachtingsBereikVoorDag(2015, cboDag.SelectedItem.ToString, Nothing)
+            Dim merkA As Bereik = merk.berekenVerwachtingsBereikVoorMerk(2015, cboMerk.SelectedItem.ToString, Nothing)
+            Dim min As Double = 100
+            Dim max As Double = 0
+            Dim bereiken As New ArrayList()
+            bereiken.AddRange({subA, dagA, merkA})
+            For j As Integer = 0 To bereiken.Count - 1
+                Dim ber As Bereik = bereiken(j)
+                If ber.getBovengrens > max Then
+                    max = ber.getBovengrens
+                End If
+                If ber.getOndergrens < min Then
+                    min = ber.getOndergrens
+                End If
+            Next
+            Dim bereik As New Bereik(min, 0, max)
+            lvi.SubItems.Add(bereik.ToString)
+            Dim dick
             If filters Is Nothing Then
-                subAfds.AddRange(sql.getArrayList("select distinct CodeSubafdeling from Cursussen where Merk = '" + cboMerk.SelectedItem.ToString + "' and dag='" + cboDag.SelectedItem.ToString + "' and year(StartDatum) = 2015 group by codesubafdeling having count(*) > 5").ToArray())
-                'TODO: catch empty arraylist
+                dick = sql.getDictionary("SELECT YEAR(c.startdatum) as jaar,count(*) as totaal,(SELECT count(*) FROM [SyntraTest].[dbo].[Cursussen] as cc WHERE cc.CodeIngetrokken = 'nee' AND CodeSubafdeling = '" + subAfds(i) + "' AND year(cc.StartDatum) = year(c.StartDatum) AND Merk = '" + cboMerk.SelectedItem.ToString + "' and dag='" + cboDag.SelectedItem.ToString + "') as nietGeschrapt FROM [SyntraTest].[dbo].[Cursussen] as c WHERE CodeSubafdeling = '" + subAfds(i) + "' AND year(c.StartDatum) =  2015 and Merk = '" + cboMerk.SelectedItem.ToString + "' and dag='" + cboDag.SelectedItem.ToString + "' group by year(startdatum)")
             Else
                 Dim fil As String
                 fil = ""
@@ -42,90 +82,29 @@
                 For Each filIt As FilterItem In filters
                     fil += " AND " + filIt.kolom + " " + filIt.factor + " " + filIt.filter
                 Next
-                If cboMerk.SelectedItem Is Nothing Then
-                    Throw New ApplicationException("Gelieve een merk te kiezen")
-                End If
-                If cboDag.SelectedItem Is Nothing Then
-                    Throw New ApplicationException("Gelieve een dag te kiezen")
-                End If
-                subAfds.AddRange(sql.getArrayList("select distinct CodeSubafdeling from Cursussen where Merk = '" + cboMerk.SelectedItem.ToString + "' and dag='" + cboDag.SelectedItem.ToString + "' and year(StartDatum) = 2015 " + fil + "group by codesubafdeling having count(*) > 5").ToArray())
-
+                dick = sql.getDictionary("SELECT YEAR(c.startdatum) as jaar,count(*) as totaal,(SELECT count(*) FROM [SyntraTest].[dbo].[Cursussen] as cc WHERE cc.CodeIngetrokken = 'nee' AND CodeSubafdeling = '" + subAfds(i) + "' AND year(cc.StartDatum) = year(c.StartDatum) AND Merk = '" + cboMerk.SelectedItem.ToString + "' and dag='" + cboDag.SelectedItem.ToString + "' " + fil + ") as nietGeschrapt FROM [SyntraTest].[dbo].[Cursussen] as c WHERE CodeSubafdeling = '" + subAfds(i) + "' AND year(c.StartDatum) =  2015 and Merk = '" + cboMerk.SelectedItem.ToString + "' and dag='" + cboDag.SelectedItem.ToString + "' " + fil + " group by year(startdatum)")
             End If
-            pgb.Minimum = 0
-            If subAfds.Count = 0 Then
-                pgb.Maximum = 1
-            Else
-                pgb.Maximum = subAfds.Count - 1
-            End If
-
-            For i As Integer = 0 To subAfds.Count - 1
-                Dim merk As New MerkBLL
-                Dim dag As New DagBll
-                Dim lvi As New ListViewItem(subAfds(i).ToString)
-                Dim subA As Bereik = subBll.berekenVerwachtingsBereikVoorSubAfd(2015, subAfds(i), Nothing)
-                Dim dagA As Bereik = dag.berekenVerwachtingsBereikVoorDag(2015, cboDag.SelectedItem.ToString, Nothing)
-                Dim merkA As Bereik = merk.berekenVerwachtingsBereikVoorMerk(2015, cboMerk.SelectedItem.ToString, Nothing)
-                Dim min As Double = 100
-                Dim max As Double = 0
-                Dim cursCount As Double = 0
-                Dim bereiken As New ArrayList()
-                bereiken.AddRange({subA, dagA, merkA})
-                For j As Integer = 0 To bereiken.Count - 1
-                    Dim ber As Bereik = bereiken(j)
-                    If ber.getBovengrens > max Then
-                        max = ber.getBovengrens
-                    End If
-                    If ber.getOndergrens < min Then
-                        min = ber.getOndergrens
-                    End If
+            Dim y As Double
+            If dick IsNot Nothing Then
+                For Each kvp As KeyValuePair(Of String, Parameter) In dick
+                    y = CDbl(kvp.Value.berekenPercentage)
                 Next
-                Dim bereik As New Bereik(min, 0, max)
-                lvi.SubItems.Add(bereik.ToString)
-                Dim dick As Dictionary(Of String, Parameter)
-                If filters Is Nothing Then
-                    dick = sql.getDictionary("SELECT YEAR(c.startdatum) as jaar,count(*) as totaal,(SELECT count(*) FROM [SyntraTest].[dbo].[Cursussen] as cc WHERE cc.CodeIngetrokken = 'nee' AND CodeSubafdeling = '" + subAfds(i) + "' AND year(cc.StartDatum) = year(c.StartDatum) AND Merk = '" + cboMerk.SelectedItem.ToString + "' and dag='" + cboDag.SelectedItem.ToString + "') as nietGeschrapt FROM [SyntraTest].[dbo].[Cursussen] as c WHERE CodeSubafdeling = '" + subAfds(i) + "' AND year(c.StartDatum) =  2015 and Merk = '" + cboMerk.SelectedItem.ToString + "' and dag='" + cboDag.SelectedItem.ToString + "' group by year(startdatum)")
-                Else
-                    Dim fil As String
-                    fil = ""
 
-                    For Each filIt As FilterItem In filters
-                        fil += " AND " + filIt.kolom + " " + filIt.factor + " " + filIt.filter
-                    Next
-                    dick = sql.getDictionary("SELECT YEAR(c.startdatum) as jaar,count(*) as totaal,(SELECT count(*) FROM [SyntraTest].[dbo].[Cursussen] as cc WHERE cc.CodeIngetrokken = 'nee' AND CodeSubafdeling = '" + subAfds(i) + "' AND year(cc.StartDatum) = year(c.StartDatum) AND Merk = '" + cboMerk.SelectedItem.ToString + "' and dag='" + cboDag.SelectedItem.ToString + "' " + fil + ") as nietGeschrapt FROM [SyntraTest].[dbo].[Cursussen] as c WHERE CodeSubafdeling = '" + subAfds(i) + "' AND year(c.StartDatum) =  2015 and Merk = '" + cboMerk.SelectedItem.ToString + "' and dag='" + cboDag.SelectedItem.ToString + "' " + fil + " group by year(startdatum)")
-                End If
+                lvi.SubItems.Add(y.ToString)
+            End If
+            Dim tf = bereik.valtTussen(y)
+            If tf Then
+                lvi.BackColor = Color.FromArgb(255, 46, 204, 113)
+                ja += 1
+            Else
+                lvi.BackColor = Color.FromArgb(255, 231, 76, 60)
+                nee += 1
+            End If
+            lvi.SubItems.Add(tf)
+            lvResult.Items.AddRange(New ListViewItem() {lvi})
 
-                If dick.ContainsKey(2015) Then
-                    cursCount = dick.Item(2015).getTotaal()
-                End If
-
-                Dim y As Double
-                If dick IsNot Nothing Then
-                    For Each kvp As KeyValuePair(Of String, Parameter) In dick
-                        y = CDbl(kvp.Value.berekenPercentage)
-                    Next
-
-                    lvi.SubItems.Add(y.ToString)
-                End If
-                Dim tf = bereik.valtTussen(y)
-                If tf Then
-                    lvi.BackColor = Color.FromArgb(255, 46, 204, 113)
-                    ja += 1
-                ElseIf bereik.getBovengrens < y Then
-                    lvi.BackColor = Color.FromArgb(255, 231, 76, 60)
-                    boven += 1
-                Else
-                    lvi.BackColor = Color.Orange
-                    nee += 1
-                End If
-                lvi.SubItems.Add(tf)
-                lvi.SubItems.Add(cursCount)
-                lvResult.Items.AddRange(New ListViewItem() {lvi})
-
-                pgb.Value = i
-            Next
-            Label1.Text = "Valt tussen: " + ja.ToString() + " valt onder: " + nee.ToString() + " valt boven " + boven.ToString
-        Catch ex As ApplicationException
-            MessageBox.Show(ex.Message)
-        End Try
+            pgb.Value = i
+        Next
+        Label1.Text = "TRUE: " + ja.ToString() + " FALSE: " + nee.ToString()
     End Sub
 End Class
