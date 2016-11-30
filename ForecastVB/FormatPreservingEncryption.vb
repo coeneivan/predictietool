24166daf64f0203f19076d508bf8226ba0f9cc0f
coeneivan <ivan.coene@student.vives.be>
2016-11-30 16:36:26 +0100

    FIX nr: 298388112305332234

--- ForecastVB/FormatPreservingEncryption.vb
+++ ForecastVB/FormatPreservingEncryption.vb
@@ -5,6 +5,6 @@ Imports DotFPE
 Public Class FormatPreservingEncryption
     Const key As Integer = 365
     Private Sub btnEncrypt_Click(sender As Object, e As EventArgs) Handles btnEncrypt.Click
-        lblEncrypted.Text = FE1.Encrypt(key, key, Encoding.UTF8.GetBytes(txtTInput.Text), Encoding.UTF8.GetBytes(txtCharacter.Text)).ToString
+        'lblEncrypted.Text = FE1.Encrypt(key, key, Encoding.UTF8.GetBytes(txtTInput.Text), Encoding.UTF8.GetBytes(txtCharacter.Text)).ToString
     End Sub
 End Class
